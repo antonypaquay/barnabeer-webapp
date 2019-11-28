7aaae031f76836310c6e27534b736c1d420243b8
Antony PAQUAY <apaquay@adneom.com>
2019-11-28 22:44:30 +0100

    style() update beers overview

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -3,71 +3,75 @@ import "./Beer.scss";
 import beerDefault from "../img/beer-default.png";
 
 class Beer extends Component {
-  handleClick = (e, key) => {
-    const beer = this.props.beers[key];
-    if (beer.beer_tasted) {
-      beer.beer_tasted = false;
-    } else {
-      beer.beer_tasted = true;
-    }
-    this.props.isTasted(key, beer);
-  };
+	handleClick = (e, key) => {
+		const beer = this.props.beers[key];
+		if (beer.beer_tasted) {
+			beer.beer_tasted = false;
+		} else {
+			beer.beer_tasted = true;
+		}
+		this.props.isTasted(key, beer);
+	};
 
-  render() {
-    const {
-      id: key,
-      beerName,
-      beerCl,
-      beerPrice,
-      beerPourcent,
-      beerType,
-      beerTasted
-    } = this.props;
+	render() {
+		const {
+			id: key,
+			beerName,
+			beerCl,
+			beerPrice,
+			beerPourcent,
+			beerType,
+			beerTasted
+		} = this.props;
 
-    if (beerTasted === true) {
-      return (
-        <React.Fragment>
-          <li
-            className="beers__list__el beers__list__el--active"
-            onClick={e => this.handleClick(e, key)}
-          >
-            <div className="beer__content">
-              <div className="beer__head">
-                <h3 className="beer__name">{beerName}</h3>
-              </div>
-              <p>Tu as déjà dégusté cette bière!</p>
-            </div>
-          </li>
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          <li
-            className="beers__list__el"
-            onClick={e => this.handleClick(e, key)}
-          >
-            <img src={beerDefault} alt="beer default" />
-            <div className="beer__content">
-              <h3 className="beer__name">{beerName}</h3>
-              <div className="beer__head">
-                <p className="beer__pourcent">{beerCl}</p>
-                <p>{beerPrice}</p>
-              </div>
-              <ul>
-                <li className="beer_type">
-                  <p>{beerPourcent}</p>
-                </li>
-                <li className="beer_type">
-                  <p>{beerType}</p>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </React.Fragment>
-      );
-    }
-  }
+		if (beerTasted === true) {
+			return (
+				<React.Fragment>
+					<li
+						className="beers__list__el beers__list__el--active"
+						onClick={e => this.handleClick(e, key)}
+					>
+						<h3 className="beer__name">{beerName}</h3>
+						<p>Tu as déjà dégusté cette bière!</p>
+					</li>
+				</React.Fragment>
+			);
+		} else {
+			return (
+				<React.Fragment>
+					<li
+						className="beers__list__el"
+						onClick={e => this.handleClick(e, key)}
+					>
+						<img src={beerDefault} alt="beer default" />
+						<h3 className="beer__name">{beerName}</h3>
+						<ul className="beer__details">
+							<li className="beer__details__li">
+								<p>
+									{beerName} <span>{beerCl} cl</span>
+								</p>
+							</li>
+							<li className="beer__details__li">
+								<p>
+									Type: <span>{beerType}</span>
+								</p>
+							</li>
+							<li className="beer__details__li">
+								<p>
+									Alcool: <span>{beerPourcent}</span>
+								</p>
+							</li>
+							<li className="beer__details__li">
+								<p>
+									Prix: <span>{beerPrice} €</span>
+								</p>
+							</li>
+						</ul>
+					</li>
+				</React.Fragment>
+			);
+		}
+	}
 }
 
 export default Beer;
