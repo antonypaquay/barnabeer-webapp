9f7b67a62090d3037f6009c8a144337cd90111d5
Antony PAQUAY <apaquay@worldofdigits.com>
2020-04-14 11:54:01 +0200

    feat(): indent

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
 
-
-
 class Profile extends Component {
-	
-
-	render() {
-		const logout = (
-			<button onClick={this.props.logOut}>
-				Me déconnecter
-			</button>
-		);
-		return (
-			<div>
-				<h1>Profile de {this.props.user}</h1>
-				<button onClick={this.props.addBeerList}>
-					Réiniliatiliser
-				</button>
-				{logout}
-			</div>
-		);
-	}
+  render() {
+    const logout = <button onClick={this.props.logOut}>Me déconnecter</button>;
+    return (
+      <div>
+        <h1>Profile de {this.props.user}</h1>
+        <button onClick={this.props.addBeerList}>Réiniliatiliser</button>
+        {logout}
+      </div>
+    );
+  }
 }
 
 export default Profile;
